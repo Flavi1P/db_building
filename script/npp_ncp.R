cef3e081c53125037ad710f3ecb44028cd062eda
Flavien Petit <flavien.petit@obs-vlfr.fr>
2025-05-28 09:53:11 +0100

    new figures and mld computation

--- script/npp_ncp.R
+++ script/npp_ncp.R
@@ -12,6 +12,9 @@ bathymetry <- st_read("C:/Users/flapet/OneDrive - NOC/Documents/NRT_viz/biocarbo
 bathymetry_2000 <- st_read("C:/Users/flapet/OneDrive - NOC/Documents/NRT_viz/biocarbon_nrt_data_viz/Data/ne_10m_bathymetry_all/ne_10m_bathymetry_I_2000.shp") |> 
   st_cast("MULTILINESTRING")
 
+
+# formatting dataset (to run only once) -----------------------------------
+
 argo <- read_parquet("data/argo_pq/biocarbon_floats_table.parquet") |> mutate(depth = round(PRES))
 
 
@@ -25,16 +28,19 @@ new_ref <- new_ref |> mutate(JULD_date = lubridate::date(JULD),
                              prof_id = paste0(PLATFORM_NUMBER, JULD_date)) |>
   filter(JULD_date > lubridate::date("2023-12-31")) |>
   filter(!prof_id %in% c("4903659 2024-06-29", "4903659 2024-06-23", "6904185 2024-01-12", "1902304 2024-01-19")) |> group_by(prof_id) %>%  # Interpolate within each profile
-  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED), 
+  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED),
                 ~ na.approx(.x, depth, na.rm = FALSE))) %>%
-  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED), 
+  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED),
                 ~ na.locf(.x, na.rm = FALSE, fromLast = TRUE))) %>%
-  mutate(across(c(NITRATE_ADJUSTED, DOXY_ADJUSTED), 
+  mutate(across(c(NITRATE_ADJUSTED, DOXY_ADJUSTED),
                 ~ na.locf(.x, na.rm = FALSE))) %>%
   ungroup()
 
 
 #write_parquet(new_ref, "data/argo_pq/float_cleaned.parquet")
+
+
+# KD490 part --------------------------------------------------------------
 
 test <- read_parquet("data/argo_pq/float_cleaned.parquet")
 
@@ -120,6 +126,8 @@ ggplot(filter(kd_profile, PRES < 40 & PRES > 10))+
 ggsave("output/plots/pp_floats/kd490_regression.png", dpi = 300, width = 20, height = 15, units = "cm")
 
 
+# NCP computation ---------------------------------------------------------
+
 npp_df <- read_csv("data/argo_pp_estimations_floats.csv") |> 
   mutate(ct = gsw_CT_from_t(PSAL, TEMP, depth),
          sigma0 = gsw_sigma0(PSAL, TEMP)) |> 
@@ -127,19 +135,26 @@ npp_df <- read_csv("data/argo_pp_estimations_floats.csv") |>
 
 
 prof_dat <- npp_df |> 
+  mutate(ct = gsw_CT_from_t(PSAL, TEMP, depth),
+         sigma0 = gsw_sigma0(PSAL, TEMP)) |> 
+  filter(PLATFORM_NUMBER == "1902304" | PLATFORM_NUMBER == "4903532") |> 
   group_by(PLATFORM_NUMBER, JULD) |> 
   mutate(nitrate_smoothed = smooth(NITRATE_ADJUSTED, k = 5, n = 2)) |> 
   summarise(MLD = mld(sigma0, depth, ref.depths=0:5, criteria = 0.03, default.depth=200),
             nitracline = clined(nitrate_smoothed, depth),
-            chl_cline = clined(CHLA_ADJUSTED, depth))
+            chl_cline = clined(CHLA_ADJUSTED, depth)) |>
+  ungroup() |> 
+  group_by(PLATFORM_NUMBER) |> 
+  mutate(delta_MLD = MLD - lag(MLD)) |> 
+  replace_na(list(delta_MLD = 0))
 
 npp_df <- left_join(npp_df, prof_dat)
 
 ggplot(npp_df)+
   geom_tile(aes(y = -depth, x = JULD, fill = NITRATE_ADJUSTED))+
-  geom_line(aes(x = JULD, y = - chl_cline))+
+  geom_line(aes(x = JULD, y = - MLD))+
   scale_fill_viridis_c()+
-  ylim(-100, 0)+
+  ylim(-300, 0)+
   facet_wrap(.~PLATFORM_NUMBER)
 
 #ggsave("output/plots/pp_floats/chla_transect_4903532.png", dpi = 300, width = 20, height = 15, units = "cm")
@@ -160,26 +175,60 @@ ggplot(npp_df)+
 
 location <- select(npp_df, JULD, LONGITUDE, LATITUDE) |> unique()
 
-integrated_df <- npp_df |> select(PLATFORM_NUMBER, JULD, pp, NITRATE_ADJUSTED) |>
-  group_by(PLATFORM_NUMBER, JULD) |> 
-  summarise_all(sum) |> 
+#Making a 200:1000 depth argo table
+
+argo <- read_parquet("data/argo_pq/biocarbon_floats_table.parquet") |> mutate(depth = round(PRES))
+
+new_ref <- select(argo, PLATFORM_NUMBER, JULD) |> unique()|> dplyr::filter(PLATFORM_NUMBER %in% c("1902304 ", "4903532 "))
+depth <- tibble("depth" = c(0:1000))
+
+new_ref <- new_ref |> crossing(depth) |> left_join(argo)
+
+new_ref <- new_ref |> mutate(JULD_date = lubridate::date(JULD),
+                             prof_id = paste0(PLATFORM_NUMBER, JULD_date)) |>
+  filter(JULD_date > lubridate::date("2023-12-31")) |>
+  filter(!prof_id %in% c("1902304 2024-01-19")) |> group_by(prof_id) %>%  # Interpolate within each profile
+  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED),
+                ~ na.approx(.x, depth, na.rm = FALSE))) %>%
+  mutate(across(c(LONGITUDE, LATITUDE, TEMP, PSAL, CHLA_ADJUSTED, BBP700_ADJUSTED, NITRATE_ADJUSTED, DOXY_ADJUSTED),
+                ~ na.locf(.x, na.rm = FALSE, fromLast = TRUE))) %>%
+  mutate(across(c(NITRATE_ADJUSTED, DOXY_ADJUSTED),
+                ~ na.locf(.x, na.rm = FALSE))) %>%
+  ungroup()
+
+new_ref$PLATFORM_NUMBER <- as.numeric(new_ref$PLATFORM_NUMBER)
+
+
+integrated_df <- npp_df |>
+  mutate(layer = case_when(depth <= MLD ~ "MLD",
+                           depth > MLD & depth <= max(prof_dat$MLD) ~ "SUB MLD",
+                           depth > max(prof_dat$MLD)~"DEEP")) |> 
+  select(PLATFORM_NUMBER, JULD, NITRATE_ADJUSTED, layer, MLD, depth) |>
+  group_by(PLATFORM_NUMBER, JULD, layer) |> 
+  summarise("integrated_nitrate" = sum(NITRATE_ADJUSTED), "mean_nitrate" = mean(NITRATE_ADJUSTED), layer_top = min(depth), layer_bottom = max(depth)) |> 
   ungroup() |> 
   left_join(prof_dat) |> 
-  left_join(location) |> 
-  group_by(PLATFORM_NUMBER) |> 
-  mutate(nitrate_smoothed = smooth.spline(JULD, NITRATE_ADJUSTED, spar = 0.6)$y)
+  group_by(PLATFORM_NUMBER, layer) |> 
+  mutate(mean_nitrate_smoothed = smooth.spline(JULD, mean_nitrate, spar = 0.6)$y,
+         integrated_nitrate_smoothed = smooth.spline(JULD, integrated_nitrate, spar = 0.6)$y)
   
 
 integrated_df$date <- lubridate::date(integrated_df$JULD)
 
 ggplot(integrated_df)+
-  geom_point(aes(x = date, y = NITRATE_ADJUSTED, color = as.factor(PLATFORM_NUMBER)))+
-  geom_path(aes(x = date, y = nitrate_smoothed, color = as.factor(PLATFORM_NUMBER)))+
+  geom_point(aes(x = date, y = mean_nitrate, color = as.factor(PLATFORM_NUMBER), shape = layer))+
+  geom_path(aes(x = date, y = mean_nitrate_smoothed, color = as.factor(PLATFORM_NUMBER), linetype = layer))+
+  scale_color_brewer(palette = "Set1", name = "wmo")
+
+
+ggplot(integrated_df)+
+  geom_point(aes(x = date, y = integrated_nitrate, color = as.factor(PLATFORM_NUMBER), shape = layer))+
+  geom_path(aes(x = date, y = integrated_nitrate_smoothed, color = as.factor(PLATFORM_NUMBER), linetype = layer))+
   scale_color_brewer(palette = "Set1", name = "wmo")
 
 #ggsave("output/plots/pp_floats/nitrate_ts.png", dpi = 300, width = 20, height = 15, units = "cm")
 
-
+ncp_ready <- left_join(npp_df, integrated_df)
 
 ggplot(integrated_df)+
   geom_point(aes(x = date, y = -nitracline))
@@ -242,7 +291,7 @@ ggplot(integrated_df)+
 
 ggplot(integrated_df)+
   geom_path(aes(x = date, y = ncp, color = as.factor(PLATFORM_NUMBER)))+
-  geom_path(aes(x = date, y = pp, color = as.factor(PLATFORM_NUMBER)), linetype = "dashed")+
+  geom_path(aes(x = date, y = pp*12, color = as.factor(PLATFORM_NUMBER)), linetype = "dashed")+
   ylab("Integrated NCP")+
   scale_color_brewer(palette = "Set1", name = "WMO")
 ggsave("output/plots/pp_floats/ncpcomparison_line.png", dpi = 300, width = 20, height = 15, units = "cm")
